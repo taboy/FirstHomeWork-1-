7cd014ab4f1746b5d51b88908ce639f79a4798d6
Taleh <t.mirzeyev@gmail.com>
2022-02-04 22:27:33 +0300

    2

---
+++ homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 6: Выяснить является ли число чётным
+Console.WriteLine("Write a number:");
+int number = Convert.ToInt16(Console.ReadLine());
+if( number % 2 == 0)
+{
+    Console.WriteLine("cislo cetnoe");
+}
+else{
+    Console.WriteLine("necotnoe");
+}
