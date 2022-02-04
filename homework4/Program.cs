0f965b4f5183f1e5cf48d357ff8340ac00fdb2a1
Taleh <t.mirzeyev@gmail.com>
2022-02-04 22:28:27 +0300

    4

---
+++ homework4/Program.cs
@@ -0,0 +1,38 @@
+﻿//Показать чётные числа от 1 до N.
+int CountA = 2;
+int CountB = 0;
+int CountC = 0;
+Console.WriteLine("please write first number");
+int FirstNum = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("please write second number");
+int SecondNum = Convert.ToInt16(Console.ReadLine());
+if(SecondNum % 2 == 0){
+    while(CountC < SecondNum -2 ){
+        if(FirstNum % 2 ==0){
+            CountC = FirstNum +CountA;
+            FirstNum = CountC;
+            Console.WriteLine(CountC);
+        }
+        else if(FirstNum % 2 == 1){
+            CountB = FirstNum - 1;
+            CountC = CountB +CountA;
+            FirstNum = CountC;
+            Console.WriteLine(CountC);
+        }
+    }
+}    
+else if(SecondNum % 2 == 1){
+    while(CountC < SecondNum -1 ){
+        if(FirstNum % 2 ==0){
+            CountC = FirstNum +CountA;
+            FirstNum = CountC;
+            Console.WriteLine(CountC);
+        }
+        else if(FirstNum % 2 == 1){
+            CountB = FirstNum - 1;
+            CountC = CountB +CountA;
+            FirstNum = CountC;
+            Console.WriteLine(CountC);
+        }
+    }
+}    
