7ee999623c120440157d6ddbfa3298b402b5b24c
Taleh <t.mirzeyev@gmail.com>
2022-02-04 22:29:39 +0300

    2

---
+++ homewrok2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 4: Найти максимальное из трёх чисел.
+Console.WriteLine ("Write first number");
+int first_num = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine ("Write second number");
+int second_num = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine ("Write third number");
+int third_num = Convert.ToInt16(Console.ReadLine());
+if(first_num > second_num && first_num > third_num)
+{
+    Console.WriteLine(first_num + " is a highest number");
+}
+else if ( second_num > first_num && second_num > third_num)
+{
+    Console.WriteLine(second_num + " is a highest number");
+
+}
+else if ( third_num > first_num && third_num > second_num)
+{
+    Console.WriteLine(third_num + " is a highest number");
+
+}
